240aef03472532c5aa47b192a0ec63e00cda716d
Marko <markoelez7@gmail.com>
2019-08-22 23:10:24 -0400

    screen stuff

--- krunkerbot.py
+++ krunkerbot.py
@@ -4,27 +4,42 @@ import cv2
 from mss import mss
 import numpy as np
 import pyautogui
-from Quartz import CGWindowListCopyWindowInfo, kCGNullWindowID, kCGWindowListOptionAll
+from Quartz import CGWindowListCopyWindowInfo, kCGNullWindowID, kCGWindowListOptionAll, CGWindowListCreateImage, CGRectMake, CGImageGetWidth, kCGWindowImageDefault, CGRectInfinite
 
-
-bbox = {'top': 100, 'left': 20, 'width': 500, 'height': 500}
 
 windows = CGWindowListCopyWindowInfo(kCGWindowListOptionAll, kCGNullWindowID)
 
 for window in windows:
     #print(window)
     try:
-        name = window['kCGWindowName']
-        if name == 'Krunker':
+        if window['kCGWindowName'] == 'Krunker':
             print('Krunker window found!')
+            print(window)
+
             # get window dimensions
             x = int(window['kCGWindowBounds']['X'])
             y = int(window['kCGWindowBounds']['Y'])
             width = int(window['kCGWindowBounds']['Width'])
             height = int(window['kCGWindowBounds']['Height'])
+            print('Dimensions: {}'.format((x, y, width, height)))
 
             center = (width//2, height//2)
-            print(center)
+            print('Center: {}'.format(center))
+
+            window_id = int(window['kCGWindowNumber'])
+            print('Window ID: {}'.format(window_id))
+
+            #region = CGRectMake(x, y, width, height)
+            region = CGRectInfinite
+            print('Region: {}'.format(region))
+
+            img = CGWindowListCreateImage(region, kCGWindowListOptionAll, window_id, kCGWindowImageDefault)
+            print('Test image width: {}'.format(CGImageGetWidth(img)))
+
+            np_img = np.array(img)
+
+
+            print(np_img)
 
 
     except:
